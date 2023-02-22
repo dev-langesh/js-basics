9494290def71c9e656ba2851a0480a64d47cf1ce
dev-langesh <dev.langesh@gmail.com>
2023-02-22 10:00:34 +0530

    variables and datatypes

---
+++ script.js
@@ -0,0 +1,16 @@
+let a = 1;
+
+let arr = [1, 2, 3, 4, 5];
+
+const obj = {
+  name: "langesh",
+  age: 19,
+  arr: [2, 3, 4],
+
+  class: {
+    no: 302,
+    block: "B",
+  },
+};
+
+console.log(typeof a);
